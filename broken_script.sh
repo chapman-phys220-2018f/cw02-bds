9c18661263443e38b4aa683c1263c421ddf42780
Enea Dodi <dodi@chapman.edu>
2018-09-12 07:29:49 +0000

    First fix

--- broken_script.sh
+++ broken_script.sh
@@ -1,12 +1,13 @@
 #/bin/bsh
 
-# You will change this line later
-eco "This script hs typs. Pleez fix them."
-# You will also change this line later
+echo "This script has typos. Please fix them."
 
 echo "Positive integers from 005 to 100:"
-for i in ${seq -w 5 100); do
-    echo i
-fi
-
+for i in $(seq -w 5 100); do
+    echo -n "$i ";
+done;
+echo
 echo "This script is now fixed."
+echo
+
+exit 0;
