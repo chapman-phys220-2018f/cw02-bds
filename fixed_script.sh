4de10aef1c2a7493d5e5bf35644a1157ea03d7e3
Enea Dodi <dodi@chapman.edu>
2018-09-12 07:36:24 +0000

    Third fix

--- fixed_script.sh
+++ fixed_script.sh
@@ -1,7 +1,8 @@
 #/bin/bsh
 
-#This is a conflicting comment
+#This is a different conflicting comment
 echo "This script has typos. Please fix them."
+#This is an unrelated comment
 
 echo "Positive integers from 005 to 100:"
 for i in $(seq -w 5 100); do
